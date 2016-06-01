1d4323dcf27ad63c8c342710b3080f24a27b7e66
mballance <matt.ballance@gmail.com>
2016-05-31 18:40:17 -0700

    ""

---
+++ rtl/axi4/axi4.f
@@ -0,0 +1,3 @@
+
+${STD_PROTOCOL_IF}/rtl/axi4/axi4_if.sv
+${STD_PROTOCOL_IF}/rtl/axi4/axi4_ace_if.sv
---
+++ rtl/rtl.f
@@ -0,0 +1,4 @@
+
+-f ${STD_PROTOCOL_IF}/rtl/axi4/axi4.f
+-f ${STD_PROTOCOL_IF}/rtl/wb/wb.f
+-f ${STD_PROTOCOL_IF}/rtl/memory/memory.f
