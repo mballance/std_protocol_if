a1a92aab71843ad452f34da88172acfa9489d5d8
Matthew Ballance <matt.ballance@gmail.com>
2017-09-15 14:21:47 -0700

    Rename makefile

--- src/std_protocol_if.mk
+++ src/std_protocol_if.mk
@@ -1,10 +1,10 @@
 
-STD_PROTOCOL_IF_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
+STD_PROTOCOL_IF_SRC_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 
 ifneq (1,$(RULES))
 
 STD_PROTOCOL_IF_LIB := std_protocol_if.jar
-STD_PROTOCOL_IF_SRC := $(wildcard $(STD_PROTOCOL_IF_DIR)/src/std_protocol_if/*.scala)
+STD_PROTOCOL_IF_SRC := $(wildcard $(STD_PROTOCOL_IF_SRC_DIR)/std_protocol_if/*.scala)
 
 else # Rules
 
